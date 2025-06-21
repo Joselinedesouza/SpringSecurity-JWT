61d75d1bc2c51cc7375af85bd7c634c95b424d14
Joselinedesouza <joselinedesouza022@gmail.com>
2025-06-21 16:38:59 +0200

    Step 6: creato AuthController per login e registrazione

---
+++ src/main/java/it/epicode/SpringSecurity/JWT/controller/AuthController.java
@@ -0,0 +1,27 @@
+package it.epicode.SpringSecurity.JWT.controller;
+
+import it.epicode.SpringSecurity.JWT.dto.AuthResponse;
+import it.epicode.SpringSecurity.JWT.dto.LoginRequest;
+import it.epicode.SpringSecurity.JWT.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
